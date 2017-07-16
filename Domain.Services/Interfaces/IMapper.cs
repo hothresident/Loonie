3c8f6410618fb66e9a4af6744eb22f7e524a4cf3
Matt <OneChickenOfDeath@gmail.com>
2017-07-15 20:36:18 -0500

    Initial Check In

--- Domain.Services/Interfaces/IMapper.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Domain.Services.Interfaces
-{
-    public interface IMapper
-    {
-        TDestinationType Map<TDestinationType>(object source);
-        TDestination Map<TSource, TDestination>(TSource source, TDestination destination);
-    }
-}
