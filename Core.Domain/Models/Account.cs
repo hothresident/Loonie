d544f9b9d36d9c6b71d6aba464506f6061557f4f
Matt <OneChickenOfDeath@gmail.com>
2017-07-22 17:59:02 -0500

    Adding new core objects

--- Core.Domain/Models/Account.cs
+++ Core.Domain/Models/Account.cs
@@ -5,7 +5,9 @@ namespace Core.Domain.Models
     public class Account
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string AccountNumber { get; set; }
+        public string Type { get; set; }
+        public string Description { get; set; }
         public IEnumerable<Transaction> Transacation { get; set; }
     }
 }
---
+++ Core.Domain/Models/ExpandedTransaction.cs
@@ -0,0 +1,11 @@
+﻿namespace Core.Domain.Models
+{
+    public class ExpandedTransaction
+    {
+        public int Id { get; set; }
+        public int TransactionId { get; set; }
+        public decimal Amount { get; set; }
+        public string Category { get; set; }
+        public string Memo { get; set; }
+    }
+}
